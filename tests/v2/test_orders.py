7032a76d175d5f9df06179d3feb79b4267c3b74e
Arrotech <arrotechdesign@gmail.com>
2018-11-21 22:59:56 -0500

    [starts #162147406] write tests for orders

--- tests/v2/test_orders.py
+++ tests/v2/test_orders.py
@@ -12,6 +12,7 @@ class TestEndpoints(unittest.TestCase):
 		self.app_context.push()
 
 		self.data = {
+			"parcel_id": "1",
 			"sender_name": "Harun",
 		    "recipient": "Peter",
 	    	"destination": "Nakuru",
@@ -21,7 +22,109 @@ class TestEndpoints(unittest.TestCase):
 	    }
 
 	def test_post(self):
-		response = self.client.post('/api/v2/parcels', data=json.dumps(self.data), content_type='application/json')
+		response = self.client.post(
+			'/api/v2/parcels', data=json.dumps(self.data), content_type='application/json')
 		result = json.loads(response.data.decode())
-		self.assertEqual(result['message'],"Order created successfully!", msg="Not allowed")
+		self.assertEqual(result["message"], 'Order created successfully!')
 		assert response.status_code == 201
+
+	def test_get(self):
+		response = self.client.get(
+			'/api/v2/parcels', data=json.dumps(self.data), content_type='application/json')
+		result = json.loads(response.data.decode())
+		self.assertEqual(result['message'],
+           'success', msg="Not allowed")
+		assert response.status_code == 200
+
+	def test_get_parcel_by_id(self):
+		response = self.client.get(
+			'/api/v2/parcels/1', data=json.dumps(self.data), content_type='application/json')
+		result = json.loads(response.data.decode())
+		assert response.status_code == 200
+
+	def test_cancel_order(self):
+		data = []
+		response = self.client.put('/api/v2/parcels/1/cancel',data=json.dumps(data), content_type='application/json')
+		result = json.loads(response.data.decode())
+		assert response.status_code == 404
+
+	def test_delete_order(self):
+		res = self.client.delete(
+			"/api/v2/parcels/1/delete", content_type='application/json')
+		self.assertEqual(res.status_code, 200)
+		self.assertEqual(json.loads(res.data)['message'], "Order deleted")
+
+	def test_no_order_delete(self):
+		res = self.client.delete(
+			"/api/v2/parcels/1111/delete", content_type='application/json')
+		self.assertEqual(res.status_code, 404)
+		self.assertEqual(json.loads(res.data)['message'], "Order Unavailable")
+
+	def test_empty_username(self):
+		data = {
+		"parcel_id": "1",
+		"sender_name": "Harun",
+		"recipient": "Peter",
+		"destination": "Nakuru",
+		"pickup": "Nairobi",
+		"weight": 2,
+		"username": ""
+		}
+
+		res = self.client.post("/api/v2/parcels",
+			data=json.dumps(data),
+			content_type='application/json')
+		result = json.loads(res.data.decode())
+		assert res.status_code == 400
+
+	def test_invalid_name(self):
+		data = {
+		"parcel_id": "1",
+		"sender_name": "******",
+		"recipient": "Peter",
+		"destination": "Nakuru",
+		"pickup": "Nairobi",
+		"weight": 2,
+		"username": "arrotech"
+		}
+
+		response = self.client.post("/api/v2/parcels", data=json.dumps(data), content_type='application/json')
+		result = json.loads(response.data.decode())
+		assert response.status_code == 400
+
+	def test_invalid_weight(self):
+		data = {
+		"parcel_id": "1",
+		"sender_name": "George",
+		"recipient": "Peter",
+		"destination": "Nakuru",
+		"pickup": "Nairobi",
+		"weight": "2",
+		"username": "arrotech"
+		}
+
+		response = self.client.post("/api/v2/parcels", data=json.dumps(data), content_type='application/json')
+		result = json.loads(response.data.decode())
+		assert response.status_code == 400
+
+	def test_key(self):
+		data = {
+		"parcel_id": "1",
+		"sender_name": "George",
+		"": "Peter",
+		"destination": "Nakuru",
+		"pickup": "Nairobi",
+		"weight": "2",
+		"username": "arrotech"
+		}
+
+		response = self.client.post("/api/v2/parcels", data=json.dumps(data), content_type='application/json')
+		result = json.loads(response.data.decode())
+		assert response.status_code == 400
+
+
+
+
+
+
+
