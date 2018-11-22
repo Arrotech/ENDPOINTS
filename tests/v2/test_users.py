41b752f5aeacdf215243683935bbd91ad8ddce02
Arrotech <arrotechdesign@gmail.com>
2018-11-22 14:18:35 -0500

    [starts #162160060] write test for users views

--- tests/v2/test_users.py
+++ tests/v2/test_users.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 from app import parcel_app
+from utils.dummy import user_register, user_login 
 
 
 class TestUsers(unittest.TestCase):
@@ -11,26 +12,33 @@ class TestUsers(unittest.TestCase):
 		self.app_context = self.app.app_context()
 		self.app_context.push()
 
-		self.user = {
-	        "email": "arrotechdesign@gmail.com",
-	        "password": "20930988",
-	        "username": "arrotech",
-	        "check_admin": "is admin"
-	    }
+		
+
+	def get_token(self):
+
+		self.client.post('/api/v2/auth/signup', data=json.dumps(user_register),
+		content_type='application/json')
+		resp = self.client.post('/api/v2/auth/login', data=json.dumps(user_login),
+			content_type='application/json')
+		access_token = json.loads(resp.get_data(as_text=True))['token']
+		auth_header = {'Authorization': 'Bearer {}'.format(access_token)}
+		return auth_header
 
 
 	def test_create_account(self):
 		response = self.client.post(
-			'/api/v2/auth/signup', data=json.dumps(self.user), content_type='application/json')
+			'/api/v2/auth/signup', data=json.dumps(user_register), content_type='application/json', headers=self.get_token())
 		result = json.loads(response.data.decode())
 		self.assertEqual(result['message'], 'Account created successfully', msg='not allowed')
 		assert response.status_code == 201
 
 	def test_signin_account(self):
 		response = self.client.post(
-			'/api/v2/auth/login', data=json.dumps(self.user), content_type='application/json')
+			'/api/v2/auth/login', data=json.dumps(user_login), content_type='application/json', headers=self.get_token())
 		result = json.loads(response.data.decode())
 		self.assertEqual(result['message'], 'successfully logged in', msg='not allowed')
 		assert response.status_code == 200
+
+
 
     
